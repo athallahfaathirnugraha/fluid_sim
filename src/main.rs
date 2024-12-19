d41a79128b91fb927747121b7b1f37f8f4c2eda0
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-12-19 20:21:47 +0700

    use eframe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("fluid sim", native_options, Box::new(|cc| Ok(Box::new(MyApp::new(cc)))));
+}
+
+#[derive(Default)]
+struct MyApp;
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> MyApp {
+        MyApp::default()
+    }
+}
+
+impl eframe::App for MyApp {
+   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Hello World!");
+       });
+   }
 }
