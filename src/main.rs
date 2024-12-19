188c8d5c048d0a06ce1904d3bfa227473c2a43b7
athallahfaathirnugraha <athallahfaathirnugraha@gmail.com>
2024-12-19 21:07:45 +0700

    particle grid

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use eframe::egui;
+use egui::*;
+
+use fluid_sim::*;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
@@ -6,18 +9,39 @@ fn main() {
 }
 
 #[derive(Default)]
-struct MyApp;
+struct MyApp {
+    particles: Vec<Particle>,
+}
 
 impl MyApp {
     fn new(cc: &eframe::CreationContext<'_>) -> MyApp {
-        MyApp::default()
+        let mut particles = vec![];
+
+        let hori_num = 30;
+        let vert_num = 20;
+        let spacing = 10;
+
+        for hori in 0..hori_num {
+            for vert in 0..vert_num {
+                particles.push(Particle {
+                    x: (hori * spacing) as f32,
+                    y: (vert * spacing) as f32,
+                });
+            }
+        }
+
+        MyApp { particles }
     }
 }
 
 impl eframe::App for MyApp {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
        egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
+           let (response, painter) = ui.allocate_painter(ui.available_size(), Sense::click_and_drag());
+
+           for &particle in &self.particles {
+               painter.circle_filled(pos2(particle.x, particle.y), 2., Color32::BLUE);
+           }
        });
    }
 }
